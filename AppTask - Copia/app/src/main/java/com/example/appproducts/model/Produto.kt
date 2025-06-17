2f34cf5ab75f16b26796a407792d72b4d9876149
edembergifpb <edemberg@ifpb.edu.br>
2025-06-17 13:55:12 -0300

    2nd

--- AppTask - Copia/app/src/main/java/com/example/appproducts/model/Produto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appproducts.model
-
-import com.google.firebase.firestore.DocumentId
-
-data class Produto(var nome: String, var preco: Double) {
-    @DocumentId
-    var id: String = ""
-    constructor() : this("", 0.0)
-}
